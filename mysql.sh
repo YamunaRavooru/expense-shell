9ae3ae794108b8601963b8f3a3660ccbe2965003
yamuna <malini.mahathi@gmail.com>
2025-01-03 12:27:02 +0530

    script for mysql server

--- mysql.sh
+++ mysql.sh
@@ -33,8 +33,9 @@ systemctl enable mysqld  &>>$LOG_FILE_NAME
 validate $? "Enabling  mysql"
 systemctl start mysqld  &>>$LOG_FILE_NAME
 validate $? "starting mysql"
-mysql -h mysql.daws82s.cloud -u root -pExpenseApp@1 -e 'show databases;'
+mysql -h mysql.daws82s.cloud -u root -pExpenseApp@1 -e 'show databases;'  &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
+then
   mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOG_FILE_NAME
   validate $? "setting root password"
 else
